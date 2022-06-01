5004a68b7061ddafe172c931588d09287fb3648a
Thayna <thaynas.s502@gmail.com>
2022-06-01 14:27:05 -0300

    finished project

---
+++ main.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import smtplib
+import email
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# importar a base de dados
+table_sales = pd.read_excel('Vendas.xlsx')
+
+# faturamento por loja
+earnings = table_sales[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+
+# quantidade de produtos vendidos por loja
+amount_products_per_store = table_sales[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+
+# ticket medio da loja
+ticket_medio = (earnings['Valor Final'] / amount_products_per_store['Quantidade']).to_frame()
+ticket_medio = ticket_medio.rename(columns={0: 'Ticket Medio'})
+# enviar um email com relatorio
+
+# variables
+
+sender_email = 'maneiramadeira@gmail.com'
+receiver_email = 'thaynas.s502@gmail.com'
+password = 'mnqmbyqordsrwfbl'
+
+# setup the parameters of the message
+msg = MIMEMultipart()
+msg['From'] = 'maneiramadeira@gmail.com'
+msg['To'] = 'thaynas.s502@gmail.com'
+msg['Subject'] = "Relatorio de Vendas"
+html = f"""
+<html>
+    <body>
+        <h2>Segundo Relatorio de Vendas dos Shoppings de São Paulo</h2>
+        <h3>Faturamento de cada loja:</h3>
+        <p>
+        {earnings.to_html()}
+        </p>
+        <br>
+        <h3>Produtos vendidos por loja:</h3>
+        <p>
+        {amount_products_per_store.to_html()}
+        </p>
+        <br>
+        <h3>Ticket medio por loja:</h3>
+        <p>
+        {ticket_medio.to_html()}
+        </p>
+        <br>
+        Dúvidas me contate, Obrigado.
+        </p>
+    </body>
+</html>
+
+"""
+
+# add in the message body and send email
+part = MIMEText(html, "html")
+msg.attach(part)
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.login(sender_email, password)
+server.sendmail(
+        sender_email, receiver_email, msg.as_string()
+    )
